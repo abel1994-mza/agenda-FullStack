f1f39af833cf82c3fe0343cd1890490ee35c8cda
Abel Contreras <lucas_65_@hotmail.es>
2025-09-06 18:52:57 +0200

    Inicio de base de datos agenda

--- backend/index.js
+++ backend/index.js
@@ -5,7 +5,7 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-let persons = [{ id: "1", name: "Abel", number: "659661750" }];
+let persons = [{ id: "1", name: "Abel", number: "65966170" }];
 
 //Obtener personas de la api
 app.get("/api/persons", (req, res) => res.json(persons));
