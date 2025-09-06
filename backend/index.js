759fd7014806cf26369333966b781ae5a584563e
Abel Contreras <lucas_65_@hotmail.es>
2025-09-06 19:44:45 +0200

    Correccion de PORT

--- backend/index.js
+++ backend/index.js
@@ -59,5 +59,5 @@ app.delete("/api/persons/:id", (req, res) => {
   res.status(204).end();
 });
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, console.log(`Servidor corriendo el PORT:${PORT}`));
