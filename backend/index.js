951b2108dd344bf90535f8cbc43caf95272b3130
Abel Contreras <lucas_65_@hotmail.es>
2025-09-09 12:57:19 +0200

    Carpeta dist copiada para servir backend-frontend juntos

--- backend/index.js
+++ backend/index.js
@@ -4,6 +4,7 @@ import cors from "cors";
 const app = express();
 app.use(express.json());
 app.use(cors());
+app.use(express.static("dist"));
 
 let persons = [{ id: "1", name: "Abel", number: "659661750" }];
 
