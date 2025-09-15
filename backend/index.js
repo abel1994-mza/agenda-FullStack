ed9f734d36fb7f49782bec1aa9432dba0077e65c
Abel Contreras <lucas_65_@hotmail.es>
2025-09-15 17:33:28 +0200

    Modificaciones en index.js para render

--- backend/index.js
+++ backend/index.js
@@ -1,14 +1,24 @@
 import express from "express";
 import cors from "cors";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 app.use(express.json());
 app.use(cors());
+app.use(express.static("dist"));
 
 let persons = [{ id: "1", name: "Abel", number: "65966170" }];
 
 //Obtener personas de la api
 app.get("/api/persons", (req, res) => res.json(persons));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "dist", "index.html"));
+});
 
 //Crear nueva persona
 app.post("/api/persons", (req, res) => {
